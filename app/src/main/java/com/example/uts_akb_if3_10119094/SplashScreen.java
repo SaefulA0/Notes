19eb0d8fbcd9418c4ed0088aa919db69b303e108
mrOctopus001 <saefulanwaroktariansah@gmail.com>
2022-06-05 22:52:55 +0700

    Third update 05/06/2022

--- app/src/main/java/com/example/uts_akb_if3_10119094/SplashScreen.java
+++ app/src/main/java/com/example/uts_akb_if3_10119094/SplashScreen.java
@@ -28,6 +28,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), IntroActivity.class));
                 finish();
             }
-        }, 3000L); //3000 L = 3 detik
+        }, 2000L); //3000 L = 2 detik
     }
 }
