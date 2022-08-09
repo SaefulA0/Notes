503974fdbdd9cbd082842afc14a168eb5c72fec5
Saeful_A <saefulanwaroktariansah@gmail.com>
2022-08-09 20:12:03 +0700

    Finish

--- app/src/main/java/com/example/uts_akb_if3_10119094/FirstFragment.java
+++ app/src/main/java/com/example/uts_akb_if3_10119094/FirstFragment.java
@@ -30,65 +30,11 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FirstFragment extends Fragment implements AdapterView.OnItemClickListener{
 
-//    ListView listView;
     DBHelper helper;
-//    ListView notelist;
-    ArrayList<String> ListItem;
-    ArrayAdapter adapter;
-    long id;
     View fragment;
     Notes notes;
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FirstFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -120,11 +66,6 @@ public class FirstFragment extends Fragment implements AdapterView.OnItemClickLi
         Intent i = new Intent(getActivity(), AddActivity.class);
         startActivity(i);
     }
-//    public void ToEditnote(){
-//        Intent intent = new Intent(getActivity(), EditActivity.class);
-//        intent.putExtra("id", notes.getId());
-//        startActivity(intent);
-//    }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int i, long x) {
